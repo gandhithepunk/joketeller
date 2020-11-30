7d5d15cea709d23d9916e74ac9cb06e8f0900899
gandhithepunk <20271668+gandhithepunk@users.noreply.github.com>
2020-11-30 10:24:10 -0800

    add options to joke arrays

--- joketeller.js
+++ joketeller.js
@@ -1,3 +1,9 @@
 const intros = ['knock, knock!', 'A horse walks into a bar.', 'what do you get when you combine a'];
-const middles = [];
-const ends = [];
+
+const knockMiddles = ['Banana', 'Leon', 'Quiche' ];
+const barMiddles = ['The shocked bartender points a finger his way in alarm and yells, “Hey!”', 'He says to the bartender "If you ask \'why the long face?\', I\'m going to kick you in the nits!"', 'He orders a drink and puts a can of pepper spray on top the bar.'];
+const combineMiddles = ['kangaroo with a donkey?', ' monster and a boy scout?', 'the Rock and E.T.?'];
+
+const knockEnds = [];
+const barEnds = ['The horse says, “You read my mind, buddy.”', 'The bartender exclaims "Woah! Woah! Woah! No need to be so coltish!"', 'The bartender proceeds to give him the drink and says “Why the strong mace?”'];
+const comineEnds = ['A Kick-Ass animal!', 'A monster that can scare old ladies across the street!', 'A Rocket!'];
