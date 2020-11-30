95321614f74f4d9dda8cecb5bbd0c4eea29fc468
gandhithepunk <20271668+gandhithepunk@users.noreply.github.com>
2020-11-30 10:35:11 -0800

    add joke arrays to objects

--- joketeller.js
+++ joketeller.js
@@ -1,9 +1,13 @@
 const intros = ['knock, knock!', 'A horse walks into a bar.', 'what do you get when you combine a'];
 
-const knockMiddles = ['Banana', 'Leon', 'Quiche' ];
-const barMiddles = ['The shocked bartender points a finger his way in alarm and yells, “Hey!”', 'He says to the bartender "If you ask \'why the long face?\', I\'m going to kick you in the nits!"', 'He orders a drink and puts a can of pepper spray on top the bar.'];
-const combineMiddles = ['kangaroo with a donkey?', ' monster and a boy scout?', 'the Rock and E.T.?'];
+let jokeMiddles = {
+    knockMiddles: ['Orange', 'Leon', 'Quiche' ],
+    barMiddles: ['The shocked bartender points a finger his way in alarm and yells, “Hey!”', 'He says to the bartender "If you ask \'why the long face?\', I\'m going to kick you in the nits!"', 'He orders a drink and puts a can of pepper spray on top the bar.'],
+    combineMiddles: ['kangaroo with a donkey?', ' monster and a boy scout?', 'the Rock and E.T.?']
+};
 
-const knockEnds = [];
-const barEnds = ['The horse says, “You read my mind, buddy.”', 'The bartender exclaims "Woah! Woah! Woah! No need to be so coltish!"', 'The bartender proceeds to give him the drink and says “Why the strong mace?”'];
-const comineEnds = ['A Kick-Ass animal!', 'A monster that can scare old ladies across the street!', 'A Rocket!'];
+let jokeEnds = {
+    knockEnds: ['Orange you glad I didn\'t say Banana!', 'Leon me when you\'re not strong!', 'Can I have a hug and a Quiche?' ],
+    barEnds: ['The horse says, “You read my mind, buddy.”', 'The bartender exclaims "Woah! Woah! Woah! No need to be so coltish!"', 'The bartender proceeds to give him the drink and says “Why the strong mace?”'],
+    combineEnds: ['A Kick-Ass animal!', 'A monster that can scare old ladies across the street!', 'A Rocket!']
+};
